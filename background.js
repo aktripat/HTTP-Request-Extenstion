5a5116b98afea0bd8a0675530b964447c4f439f5
aktripat <Abhimanyu@Abhimanyus-MacBook-Pro.local>
2017-01-22 12:14:33 -0800

    HTTP Request

---
+++ background.js
@@ -0,0 +1,23 @@
+
+chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    //var newURL = "https://eee.uci.edu/myeee";
+    //chrome.tabs.create({ url: newURL });
+    //chrome.extension.getBackgroundPage().console.log('foo');
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", true);
+    xhr.onreadystatechange = function()
+    {
+      if (xhr.readyState == 4)
+      {
+    // WARNING! Might be injecting a malicious script!
+    document.getElementById("resp").innerHTML = xhr.responseText;
+    //...
+      }
+    }
+xhr.send();
+
+    console.log("HTTP request")
+    //closeTab();
+}
+);
