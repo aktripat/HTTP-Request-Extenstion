9dcc94123659fdee69ef1af9bae8c238c9ce4efe
aktripat <Abhimanyu@Abhimanyus-MacBook-Pro.local>
2017-01-31 12:48:19 -0800

    new URL + http request

--- background.js
+++ background.js
@@ -1,9 +1,10 @@
 
 chrome.browserAction.onClicked.addListener(function(activeTab)
 {
-    //var newURL = "https://eee.uci.edu/myeee";
-    //chrome.tabs.create({ url: newURL });
-    //chrome.extension.getBackgroundPage().console.log('foo');
+    var newURL = "www.google.com";
+    chrome.tabs.create({ url: newURL });
+    chrome.extension.getBackgroundPage().console.log(newURL);
+    console.log(activeTab.id)
     var xhr = new XMLHttpRequest();
     xhr.open("GET", "https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json", true);
     xhr.onreadystatechange = function()
@@ -11,7 +12,9 @@ chrome.browserAction.onClicked.addListener(function(activeTab)
       if (xhr.readyState == 4)
       {
     // WARNING! Might be injecting a malicious script!
-    document.getElementById("resp").innerHTML = xhr.responseText;
+    //document.getElementById("resp").innerHTML = xhr.responseText;
+    console.log(xhr.responseText);
+
     //...
       }
     }
